43364d190da255cc545e01ebf057a22a1d15731c
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-03-26 21:52:43 +0530

    create blog post comment section

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail"; 
+import ApprovalCard from "./ApprovalCard";
+
+
+
+
+const App = ()=>{
+   
+    return (
+        <div className="ui container comments">
+        <ApprovalCard>
+            <div>
+                <h4>Warning</h4>
+                <p>
+        
+        </div>
+        </ApprovalCard>
+            <ApprovalCard>
+            <CommentDetail author="sam" time="Today at 6.00 PM" content="nice blog post!"  /></ApprovalCard>
+             {/* <CommentDetail author="sam" time="Today at 6.00 PM" content="nice blog post!"  avatar={faker.image.avatar()}/> */}
+            <ApprovalCard><CommentDetail author="Alex" time="Today at 12.00 PM" content="keep it up" /></ApprovalCard>
+            <ApprovalCard>CommentDetail author="Jane" time="yesterday at 6.00 PM" content="great job" /></ApprovalCard>
+
+        </div>
+            
+    );
+};
+
+ReactDOM.render(<App/>,document.querySelector('#root'))
